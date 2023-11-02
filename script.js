22a15ee913401750ad318d56cb7180115d2ce625
OrkhanA <70017511+webofpies@users.noreply.github.com>
2023-11-02 19:29:47 +0400

    added keyboard clicks -not finished

--- script.js
+++ script.js
@@ -8,8 +8,26 @@ let operationUsed = false;
 let lastElement = null;
 let lastElementIsZero = false;
 let solved = false;
-const operBtns = [".plus", ".minus", ".multiply", ".divide"];
-const digitBtns = document.querySelectorAll(".digit");
+// const operBtns = [".plus", ".minus", ".multiply", ".divide"];
+// const digitBtns = document.querySelectorAll(".digit");
+const operBtns = [
+  document.querySelector(".plus"),
+  document.querySelector(".minus"),
+  document.querySelector(".multiply"),
+  document.querySelector(".divide"),
+];
+const digitBtns = [
+  document.querySelector(".zero"),
+  document.querySelector(".one"),
+  document.querySelector(".two"),
+  document.querySelector(".three"),
+  document.querySelector(".four"),
+  document.querySelector(".five"),
+  document.querySelector(".six"),
+  document.querySelector(".seven"),
+  document.querySelector(".eight"),
+  document.querySelector(".nine"),
+];
 const display = document.querySelector(".display-text");
 const equalsBtn = document.querySelector(".equals");
 const clearBtn = document.querySelector(".clear");
@@ -20,57 +38,94 @@ const operSymbolToString = {
   "/": "divide",
 };
 
+// active
+const addActiveStyleDigits = function (element) {
+  element.style.cssText =
+    "background-color: #343a40; color: #f1f3f5; cursor: pointer;";
+};
+
+// release
+const addDefaultStyleDigits = function (element) {
+  element.style.cssText =
+    "background-color: #e9ecef; color: #495057; cursor: pointer;";
+};
+
+// hover
+const addHoverStyleDigits = function (element) {
+  element.style.cssText =
+    "background-color: #f8f9fa; border: 2px solid #dee2e6; cursor: pointer;";
+};
+
+const addDigit = function (input) {
+  // console.log(operationUsed);
+  // if (solved) {
+  //   // console.log("asdasd");
+  //   x = null;
+  // displayText = input;
+  // display.textContent = displayText;
+  // }
+
+  if (operationUsed) {
+    if (y) input = `${y}${input}`;
+
+    y = Number(input);
+
+    displayText = `${x}${operation}${y}`;
+  } else {
+    if (x) input = `${x}${input}`;
+
+    x = Number(input);
+
+    displayText = x;
+  }
+
+  display.textContent = displayText;
+};
+
+const addOperator = function (input) {
+  // if there's X this means we already have two values, and need to solve
+  if (x != null && y != null) {
+    x = solve();
+    y = null;
+  }
+
+  // do not add operation symbol if no digits
+  if (displayText) {
+    // get operation after because if we have x and y present
+    // this means we already have an operation in memory
+    operation = input;
+    operationUsed = true;
+
+    displayText = `${x}${operation}`;
+    display.textContent = displayText;
+  }
+};
+
+const addEquals = function () {
+  if (operationUsed) {
+    solve();
+    operationUsed = false;
+  }
+};
+
 digitBtns.forEach((element) =>
   element.addEventListener("click", (e) => {
     let input = e.target.textContent;
 
-    if (solved) {
-      reset();
-    }
-
-    if (operationUsed) {
-      if (y) input = `${y}${input}`;
-      y = Number(input);
-
-      displayText = `${x}${operation}${y}`;
-    } else {
-      if (x) input = `${x}${input}`;
-      x = Number(input);
-
-      displayText = x;
-    }
-
-    display.textContent = displayText;
+    addDigit(input);
   })
 );
 
-// add event for operation buttons
 operBtns.forEach((element) => {
-  let btn = document.querySelector(element);
-  btn.addEventListener("click", (e) => {
-    // if there's X this means we already have two values, and need to solve
-    if (x && y) {
-      x = solve();
-      y = null;
-    }
-    // get operation after because if we have x and y present
-    // this means we already have an operation in memory
-    operation = e.target.textContent;
+  element.addEventListener("click", (e) => {
+    let input = e.target.textContent;
 
-    // do not add operation symbol if no digits
-    if (displayText) {
-      operationUsed = true;
-
-      displayText = `${x}${operation}`;
-      display.textContent = displayText;
-    }
+    addOperator(input);
   });
 });
 
-equalsBtn.addEventListener("click", (e) => {
-  if (operationUsed) {
-    solve();
-  }
+equalsBtn.addEventListener("click", () => {
+  addEquals();
 });
 
 clearBtn.addEventListener("click", (e) => {
@@ -79,16 +134,82 @@ clearBtn.addEventListener("click", (e) => {
   reset();
 });
 
-const solve = function () {
-  // y = Number(displayText.replace(`${x}${operation}`, ""));
+// mouse click, hover, active - digit buttons
+digitBtns.forEach((element) => {
+  element.addEventListener("mousedown", function () {
+    addActiveStyleDigits(element);
+  });
+});
 
+digitBtns.forEach((element) => {
+  element.addEventListener("mouseup", function () {
+    addDefaultStyleDigits(element);
+  });
+});
+
+digitBtns.forEach((element) => {
+  element.addEventListener("mouseover", function () {
+    addHoverStyleDigits(element);
+  });
+});
+
+digitBtns.forEach((element) => {
+  element.addEventListener("mouseout", function () {
+    addDefaultStyleDigits(element);
+  });
+});
+
+// keyboard clicks
+document.addEventListener("keydown", function (e) {
+  const key = e.key;
+
+  if (key !== undefined) {
+    if (!isNaN(key)) {
+      addDigit(key);
+    }
+    // else if (key === "Backspace")
+    else if (key == "+" || key == "-" || key == "*" || key == "/") {
+      addOperator(key);
+    } else if (key == "=" || key == "Enter") solve();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  const key = e.key;
+
+  if (key !== undefined) {
+    digitBtns.forEach((element) => {
+      if (key == digitBtns.indexOf(element)) {
+        addActiveStyleDigits(element);
+      }
+    });
+  }
+});
+
+document.addEventListener("keyup", function (e) {
+  const key = e.key;
+
+  if (key !== undefined) {
+    digitBtns.forEach((element) => {
+      if (key == digitBtns.indexOf(element)) {
+        addDefaultStyleDigits(element);
+      }
+    });
+  }
+});
+
+const solve = function () {
+  // override x
   let result = operate(x, y, operSymbolToString[operation]);
-  // console.log(result);
-  displayText = String(result);
+
+  displayText = parseFloat(result.toPrecision(8));
   display.textContent = displayText;
+
   solved = true;
-  // lastElement = result;
-  // reset();
+
+  x = result;
+  y = null;
+
   return result;
 };
 
@@ -105,7 +226,8 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-  return x / y;
+  if (y == 0) return "cannot divide by zero";
+  else return x / y;
 }
 
 function operate(x, y, operation) {
@@ -124,6 +246,5 @@ function reset() {
   y = null;
   operation = null;
   operationUsed = false;
-  // lastElement = null;
   solved = false;
 }
